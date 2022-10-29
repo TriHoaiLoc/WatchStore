1559dfe69e9a67f705be0f5ea472d12206381143
TriHoaiLoc <20133063@student.hcmute.edu.vn>
2022-10-29 21:42:39 +0700

    khach hang

--- Poject_QUANLYCUAHANGDONGHO/Form/FormCustomer.Designer.cs
+++ Poject_QUANLYCUAHANGDONGHO/Form/FormCustomer.Designer.cs
@@ -29,10 +29,17 @@ namespace Project_QUANLYCUAHANGDONGHO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCustomer";
+            this.Name = "FormCustomer";
+            this.Text = "Khách hàng";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
